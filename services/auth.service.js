82f8301e97a922811db81c24e937afef9efc2960
Alejandro Company <alejandrocompany@planetahuerto.es>
2023-01-17 21:57:59 +0100

    login/register esqueleton

---
+++ services/auth.service.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+const API_URL = 'http://localhost:8000/'
+
+class AuthService {
+  async registerUser(user) {
+    try {
+      const response = await axios.post(API_URL + 'users', user)
+      return response.data
+    } catch (error) {
+      this.$log.error(error)
+    }
+  }
+
+  async loginUser(user) {
+    try {
+      const response = await axios.post(API_URL + 'login', user)
+      return response.data
+    } catch (error) {
+      this.$log.error(error)
+    }
+  }
+}
+
+export default new AuthService()
---
+++ store/auth.js
@@ -0,0 +1,47 @@
+import AuthService from '@/services/auth.service'
+
+export const state = () => ({
+  user: {},
+  accessToken: '',
+})
+
+export const mutations = {
+  setAccessToken(state, accessToken) {
+    state.accessToken = accessToken
+  },
+  setUser(state, user) {
+    state.user = user
+  },
+  deleteAccessToken(state) {
+    state.accessToken = ''
+  },
+}
+
+export const actions = {
+  setAccessToken({ commit }, accessToken) {
+    commit('setAccessToken', accessToken)
+  },
+
+  logoutUser({ commit }) {
+    commit('deleteAccessToken')
+  },
+
+  async register({ commit }, user) {
+    return await AuthService.registerUser(user)
+  },
+
+  async login({ commit, dispatch }, user) {
+    const data = await AuthService.loginUser(user)
+    if (data?.token !== '') {
+      dispatch('setAccessToken', data.token)
+      commit('setUser', data.user)
+    }
+    return data
+  },
+}
+
+export const getters = {
+  isLogin: (state) => {
+    return !!state.accessToken
+  },
+}
