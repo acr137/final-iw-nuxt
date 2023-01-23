9e5e2398f635afdea07ed5b5ecbc69d9abe60cf2
Eugenio Benito <eugeniobenito00@gmail.com>
2023-01-23 11:55:51 +0100

    Include editForm into userAccountPanel

--- components/panelboards/userAccountPanel.vue
+++ components/panelboards/userAccountPanel.vue
@@ -1,28 +1,50 @@
 <template>
-<div>
-    <div v-for="(user, index) in users" :key="index">
-    <p>castañas</p>
+  <div>
+    <div class="flex items-center mx-1.5 mb-5">
+      <h1 class="ml-16 text-4xl font-bold text-gray-800 font-oswald">
+        Mis datos
+      </h1>
     </div>
-</div>
+    <div class="flex items-center justify-center mb-4">
+      <div
+        :class="[
+          'mx-16 relative w-full px-6 pt-5 pb-4 overflow-hidden transition-all transform bg-white rounded-lg shadow-xl sm:max-w-lg sm:p-6',
+        ]"
+      >
+        <edit-form />
+      </div>
+      <div
+        :class="[
+          'mx-16 relative w-full px-6 pt-5 pb-4 overflow-hidden transition-all transform bg-white rounded-lg shadow-xl sm:max-w-lg sm:p-6',
+        ]"
+      >
+        <edit-form />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import EditForm from '@/components/forms/editForm.vue'
+
 export default {
-    name: 'UserAccountPanel',
-    data() {
-        return {
-        users: [
-            {
-            name: 'Alejandro',
-            surname: 'Company',
-            },
-            {
-            name: 'Hola',
-            surname: 'Company',
-            },
-        ],
-        }
-    },
+  name: 'UserAccountPanel',
+  components: {
+    EditForm,
+  },
+  data() {
+    return {
+      users: [
+        {
+          name: 'Alejandro',
+          surname: 'Company',
+        },
+        {
+          name: 'Hola',
+          surname: 'Company',
+        },
+      ],
+    }
+  },
 }
 </script>
-  
