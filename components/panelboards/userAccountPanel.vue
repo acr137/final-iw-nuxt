4cbe5688fbedd51d8987707e26d759e1da2c1182
Eugenio Benito <eugeniobenito00@gmail.com>
2023-01-23 13:19:29 +0100

    Add user info into panel

--- components/panelboards/userAccountPanel.vue
+++ components/panelboards/userAccountPanel.vue
@@ -5,14 +5,42 @@
         Mis datos
       </h1>
     </div>
-    <div class="flex items-center justify-center mb-4">
+    <div class="flex justify-center mb-4">
       <div
-        :class="[
-          'mx-16 relative w-full px-6 pt-5 pb-4 overflow-hidden transition-all transform bg-white rounded-lg shadow-xl sm:max-w-lg sm:p-6',
-        ]"
+        class="relative flex items-center justify-center w-full px-6 pt-5 pb-4 mx-16 overflow-hidden transition-all transform bg-white rounded-lg shadow-xl sm:max-w-lg sm:p-6"
       >
-        <edit-form />
+        <div class="w-full p-10">
+          <div
+            class="flex items-center w-40 h-40 m-auto bg-black rounded-full shadow-md "
+          >
+            <p class="m-auto text-6xl font-medium text-center text-white">{{initial}}</p>
+          </div>
+
+          <!-- line divider -->
+          <div class="w-full mt-3 border-t border-gray-300"></div>
+          <!-- USER NAME -->
+          <div class="flex w-full p-1.5">
+            <p class="text-base font-medium uppercase">Nombre:</p>
+            <p class="ml-2">{{ user.name }}</p>
+          </div>
+          <!-- USER EMAIL -->
+          <div class="flex w-full p-1.5 mb-1.5">
+            <p class="text-base font-medium uppercase">Email:</p>
+            <p class="ml-2">{{ user.email }}</p>
+          </div>
+          <!-- USER PASSWORD -->
+          <div class="flex w-full p-1.5 mb-1.5">
+            <p class="text-base font-medium uppercase">Contraseña:</p>
+            <p class="ml-2">{{ maskedPassword }}</p>
+          </div>
+          <!-- USER COMPANY -->
+          <div class="flex w-full p-1.5 mb-3">
+            <p class="text-base font-medium uppercase">Mi Empresa:</p>
+            <p class="ml-2">{{ user.company }}</p>
+          </div>
+        </div>
       </div>
+
       <div
         :class="[
           'mx-16 relative w-full px-6 pt-5 pb-4 overflow-hidden transition-all transform bg-white rounded-lg shadow-xl sm:max-w-lg sm:p-6',
@@ -34,17 +62,23 @@ export default {
   },
   data() {
     return {
-      users: [
-        {
-          name: 'Alejandro',
-          surname: 'Company',
-        },
-        {
-          name: 'Hola',
-          surname: 'Company',
-        },
-      ],
+      user: {
+        name: 'Alejandro',
+        company: 'Company',
+        email: 'alejandro@example.com',
+        password: '123456789',
+      },
     }
+  },
+  computed: {
+    maskedPassword() {
+      return (
+        '*'.repeat(this.user.password.length - 2) + this.user.password.slice(-2)
+      )
+    },
+    initial() {
+      return this.user.name.match(/^[A-Z]/)[0]
+    },
   },
 }
 </script>
