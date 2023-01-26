f490dddc474d2f67c31732af2baad9a581b911d9
Alejandro Company <alejandrocompany@planetahuerto.es>
2023-01-26 21:44:46 +0100

    delete resps

--- services/tickets.service.js
+++ services/tickets.service.js
@@ -29,8 +29,7 @@ class TicketsService {
     const url = API_URL + 'tpvv/borrarTicket'
 
     try {
-      const response = await axios.delete(url, { params })
-      return response
+      await axios.delete(url, { params })
     } catch (error) {
       console.log(error)
     }
@@ -40,10 +39,9 @@ class TicketsService {
     const url = API_URL + 'tpvv/crearTicket'
 
     try {
-      const response = await axios.post(url, data.ticket, {
+      await axios.post(url, data.ticket, {
         params: { token: data.token },
       })
-      return response.data
     } catch (error) {
       console.log(error)
     }
@@ -53,10 +51,9 @@ class TicketsService {
     const url = API_URL + 'tpvv/valorarTicket'
 
     try {
-      const response = await axios.post(url, data.rating, {
+      await axios.post(url, data.rating, {
         params: { token: data.token, idTicket: data.idTicket },
       })
-      return response.data
     } catch (error) {
       console.log(error)
     }
@@ -66,8 +63,7 @@ class TicketsService {
     const url = API_URL + 'tpvv/chatTicket'
 
     try {
-      const response = await axios.get(url, { params })
-      return response.data
+      await axios.get(url, { params })
     } catch (error) {
       console.log(error)
     }
@@ -77,10 +73,9 @@ class TicketsService {
     const url = API_URL + 'tpvv/chatTicket/enviarMessage'
 
     try {
-      const response = await axios.post(url, data.message, {
+      await axios.post(url, data.message, {
         params: { token: data.token, idTicket: data.idTicket },
       })
-      return response.data
     } catch (error) {
       console.log(error)
     }
